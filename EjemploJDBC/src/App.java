3638a8294909f43f8dfd14760f210cf24f5dd721
Dany Diaz <danielferdiazz@gmail.com>
2025-03-28 14:13:06 -0300

    Ejemplo JDBC + Singleton + MVC + DAO

---
+++ EjemploJDBC/src/App.java
@@ -0,0 +1,22 @@
+import config.DBConnection;
+import controller.EstudianteController;
+import dao.EstudianteDAO;
+import view.EstudianteVista;
+
+public class App {
+    public static void main(String[] args) {
+        EstudianteDAO estudianteDAO = new EstudianteDAO();
+        EstudianteVista estudianteVista = new EstudianteVista();
+        EstudianteController controlador = new EstudianteController(estudianteDAO, estudianteVista);
+
+        // Pruebas CRUD
+        controlador.agregarEstudiante("Juan Pérez", 20);
+        controlador.listarEstudiantes();
+        //controlador.actualizarEstudiante(1, "Juan Pérez Modificado", 21);
+        //controlador.listarEstudiantes();
+        //controlador.eliminarEstudiante(1);
+        //controlador.listarEstudiantes();
+
+        DBConnection.getInstance().cerrarConexion();
+    }
+}
